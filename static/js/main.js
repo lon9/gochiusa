c6c9e5b616bd1b22c82e89d2c9c85d4c97b1a0d2
Rompei <rompei0924@gmail.com>
2015-12-09 23:07:08 +0900

    Add image for smart devices.

--- static/js/main.js
+++ static/js/main.js
@@ -7,8 +7,32 @@ $(function(){
   var finishVideoLoad = false;
   var finishGetCount = false;
 
+  var mobileImageURLs = [
+    "http://livedoor.blogimg.jp/anihonet/imgs/b/8/b81abec0-s.jpg",
+    "http://pic.prepics-cdn.com/yuka09166190/33027011.jpeg",
+    "https://divnil.com/wallpaper/iphone5s/img/app/1/3/1392_744_goichiusa_107_9b7370be5cbe06423c83c1e673d6b17c_raw.jpg",
+    "http://pic.prepics-cdn.com/animealbum/36279839.jpeg",
+    "https://divnil.com/wallpaper/iphone5s/img/app/2/0/201406241903502c9_72a723c77485f12560d4951f9cd2f3ac_raw.jpg",
+    "http://blog-imgs-65.fc2.com/h/a/t/hatsunemiku1006/201406241859522f4.jpg",
+    "http://blog-imgs-65.fc2.com/h/a/t/hatsunemiku1006/20140624185951fb1.jpg",
+    "http://pic.prepics-cdn.com/orderrabbit/32552177.jpeg",
+    "http://www.gochiusa.com/core_sys/images/main/cont/news/sp_v/960/syaro.jpg"
+  ];
+
+  var tabletImageURLs = [
+    "http://livedoor.blogimg.jp/shachiani/imgs/a/e/aedfc928.jpg",
+    "http://blog-imgs-65.fc2.com/h/a/t/hatsunemiku1006/201406241903482c5.jpg",
+    "http://blog-imgs-65.fc2.com/h/a/t/hatsunemiku1006/20140624190346915.jpg",
+    "http://blog-imgs-65.fc2.com/d/g/d/dgdg875/anime_wallpaper_Gochumon_wa_Usagi_Desu_ka_Hoto_Kokoa_Kafuu_Chino_Natsu_Megumi-77886.png"
+  ];
+
   if(_ua.Mobile || _ua.Tablet){
     finishVideoLoad = true;
+    if(_ua.Mobile){
+      $('body').css('background-image', 'url(' + mobileImageURLs[Math.floor(Math.random() * mobileImageURLs.length)] + ')');
+    }else if(_ua.Tablet){
+      $('body').css('background-image', 'url(' + tabletImageURLs[Math.floor(Math.random() * tabletImageURLs.length)] + ')');
+    }
   }
 
   // Ajax
